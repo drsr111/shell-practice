73f50bd6b6f08684e7c21df94e6347aaee0c18b5
drsr111 <drsr.111@gmail.com>
2022-08-12 08:58:12 +0530

    input4

--- hello2.sh
+++ hello2.sh
@@ -1,6 +1,6 @@
 echo "Enter name: "
 read name1 name2 name3
 echo "entered name1: $name1"
-echo "entered name1: $name2"
-echo "entered name1: $name3"
+echo "entered name2: $name2"
+echo "entered name3: $name3"
 
